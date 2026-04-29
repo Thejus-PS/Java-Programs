768d21e46759ad70c50b669297b43b301c7cb1a7
Thejus <thejusps2001@gmail.com>
2026-04-29 23:13:01 +0530

    Added program to reverse a string

---
+++ src/strings/ReverseString.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Thejus";
+		String reversed ="";
+		
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			reversed = reversed+str.charAt(i);
+		}
+		System.out.println("Reversed string is:"+reversed);
+	}
+}
